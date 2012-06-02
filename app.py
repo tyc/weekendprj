b63cbb250a5c66ac22c7d9a6b7ddc5fa13cf4163
Tehn Yit Chin <tehn.yit.chin@gmail.com>
2012-06-02 23:39:03 +0200

    Added shebang so it can be called directly from the command line.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import datetime
 import os
 import subprocess
@@ -8,7 +9,6 @@ import hashlib
 APP_ROOT = os.path.dirname(os.path.realpath(__file__))
 MEDIA_ROOT = os.path.join(APP_ROOT, 'static')
 MEDIA_URL = '/static/'
-PASSWORD = 'shh'
 PHANTOM = '/usr/local/bin/phantomjs'
 SCRIPT = os.path.join(APP_ROOT, 'screenshot.js')
 
@@ -19,7 +19,7 @@ parser.add_option("-u", "--url", dest="url", help="url to get a snapshot of")
 if options.url != None:
         url = str(options.url)
 else:   
-        url = "www.hp.com"
+        url = "http://www.hp.com"
 print "getting snapshot of " + url
 
 filename = "bookmarks-" + (hashlib.md5(url).hexdigest()) + ".png" 
