a5c511e89344b911ecdb1de2cdd8958709a73f7c
Tehn Yit Chin <tehn.yit.chin@gmail.com>
2012-06-02 23:01:28 +0200

    added screen capture function

---
+++ app.py
@@ -0,0 +1,29 @@
+import datetime
+import os
+import subprocess
+from optparse import OptionParser
+import hashlib
+
+# app configuration
+APP_ROOT = os.path.dirname(os.path.realpath(__file__))
+MEDIA_ROOT = os.path.join(APP_ROOT, 'static')
+MEDIA_URL = '/static/'
+PASSWORD = 'shh'
+PHANTOM = '/usr/local/bin/phantomjs'
+SCRIPT = os.path.join(APP_ROOT, 'screenshot.js')
+
+parser = OptionParser()
+parser.add_option("-u", "--url", dest="url", help="url to get a snapshot of")
+(options, args)=parser.parse_args()
+
+if options.url != None:
+        url = str(options.url)
+else:   
+        url = "www.hp.com"
+print "getting snapshot of " + url
+
+filename = "bookmarks-" + (hashlib.md5(url).hexdigest()) + ".png" 
+outfile = os.path.join(MEDIA_ROOT, filename)
+params = [PHANTOM, SCRIPT, url, outfile]
+
+exitcode = subprocess.call(params)
